de7a7e57b96ee03005962669d0c5e3f8cd292e31
ezecernadas <ezecernadas@gmail.com>
2025-07-17 14:50:39 -0300

    mas tarjetas

--- views/explorar/explorar.php
+++ views/explorar/explorar.php
@@ -59,6 +59,7 @@
                 </div>
             </section>
         </section>
+        <h2 class="vistos_recientemente">Roberto Arlt</h2>
         <section class="contenido">
             <article class="apunte">
                 <figure>
@@ -114,6 +115,171 @@
                 </section>
             </article>
 
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Funciones Cuadráticas</h2>
+                    <p>Matemática - Orona Nicolas</p>
+                    <p>2024-09-02 - 4.3</p>
+                </section>
+            </article>
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Leyes de Newton</h2>
+                    <p>Física - Isaac Gómez</p>
+                    <p>2025 - 4.8</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Realismo mágico</h2>
+                    <p>Literatura - María Torres</p>
+                    <p>2024 - 4.2</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Constitución Argentina</h2>
+                    <p>Ciudadanía - Prof. Salazar</p>
+                    <p>2025 - 4.6</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Bases de HTML y CSS</h2>
+                    <p>Informática - Lic. Ramírez</p>
+                    <p>2025 - 5.0</p>
+                </section>
+            </article>
+
+
+        </section>
+        <br>
+        <h2 class="para_ti">Fatima</h2>
+        <section class="contenedor_apuntes">
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Funciones Cuadráticas</h2>
+                    <p>Matemática - Orona Nicolas</p>
+                    <p>2024-09-02 - 4.3</p>
+                </section>
+            </article>
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Leyes de Newton</h2>
+                    <p>Física - Isaac Gómez</p>
+                    <p>2025 - 4.8</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Realismo mágico</h2>
+                    <p>Literatura - María Torres</p>
+                    <p>2024 - 4.2</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Constitución Argentina</h2>
+                    <p>Ciudadanía - Prof. Salazar</p>
+                    <p>2025 - 4.6</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Bases de HTML y CSS</h2>
+                    <p>Informática - Lic. Ramírez</p>
+                    <p>2025 - 5.0</p>
+                </section>
+            </article>
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Funciones Cuadráticas</h2>
+                    <p>Matemática - Orona Nicolas</p>
+                    <p>2024-09-02 - 4.3</p>
+                </section>
+            </article>
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Leyes de Newton</h2>
+                    <p>Física - Isaac Gómez</p>
+                    <p>2025 - 4.8</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Realismo mágico</h2>
+                    <p>Literatura - María Torres</p>
+                    <p>2024 - 4.2</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Constitución Argentina</h2>
+                    <p>Ciudadanía - Prof. Salazar</p>
+                    <p>2025 - 4.6</p>
+                </section>
+            </article>
+
+            <article class="apunte">
+                <figure>
+
+                </figure>
+                <section class="informacion">
+                    <h2>Bases de HTML y CSS</h2>
+                    <p>Informática - Lic. Ramírez</p>
+                    <p>2025 - 5.0</p>
+                </section>
+            </article>
         </section>
     </main>
 
