4712ebd6a9b91ae9d2d0d4ffe3352eb5069d4402
Milee025 <milenalopez937@gmail.com>
2025-07-16 23:24:46 -0300

    explorar modificado

---
+++ views/explorar/componentes/apunte_explorar.php
@@ -0,0 +1,11 @@
+    <article class="apunte">
+        <figure>
+    
+        </figure>
+        <section class="informacion">
+            <h2>Funciones Cuadráticas</h2>
+            <p>Matemática - Fatima</p>
+            <p>2024-09-02 -  4.3</p>
+        </section>
+    </article>
+
