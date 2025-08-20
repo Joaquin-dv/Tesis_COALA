0645766abbeab957e5769956ada1aa9281f0ddd2
ezecernadas@gmail.com <ezecernadas>
2025-08-20 18:17:21 -0300

    Ticket 0005 Fix * explorarController.php - Se hizo la lógica para levantar el componente apunte, cargarlo con datos y mostrarlo en la sección Explorar * inicioController.php - Se cambio la fecha de 2025 a 02/08/2025 * explorarView.html - Se quitaron los apuntes hardcodeados y se puso la variable de Mopla para cargar los componentes ahí.

--- controllers/inicioController.php
+++ controllers/inicioController.php
@@ -1,63 +1,65 @@
 <?php
 
-$tpl = new Mopla('inicio');
+	$tpl = new Mopla('inicio');
 
-// Se levanta el componente
-$componente_apunte = new Mopla('components/tarjeta_apunte');
+	// Se levanta el componente
+	$componente_apunte = new Mopla('components/tarjeta_apunte');
 
-// Variable donde se guardan los apuntes con los datos cargados
-$lista_apuntes_recientes = "";
+	// Variable donde se guardan los apuntes con los datos cargados
+	$lista_apuntes_recientes = "";
 
-// ====== VISTOS RECIENTEMENTE ======
+	// ====== VISTOS RECIENTEMENTE ======
 
-// Carga de info en componentes (debe ser dinámica, osea un foreach con la info que trajo el modelo)
-for ($i = 0; $i < 10; $i++) {
-	// Variables de prueba para los apuntes
-	$vars = [
-		"IMAGEN" => "",
-		"TITULO" => "Titulo",
-		"MATERIA" => "Historia",
-		"ESCUELA" => "Roberto Arlt",
-		"AÑO" => "2025",
-		"PUNTUACION" => "4.5"
-	];
+	// Carga de info en componentes (debe ser dinámica, osea un foreach con la info que trajo el modelo)
+	for ($i = 0; $i < 10; $i++) {
+		// Variables de prueba para los apuntes
+		$vars = [
+			"IMAGEN" => "",
+			"TITULO" => "Titulo",
+			"MATERIA" => "Historia",
+			"ESCUELA" => "Roberto Arlt",
+			"AÑO" => "02/08/2025",
+			"PUNTUACION" => "4.5"
+		];
 
-	// Se carga la info en el buffer componente
-	$componente_apunte->setVars($vars);
+		// Se carga la info en el buffer componente
+		$componente_apunte->setVars($vars);
 
-	// Se guarda el buffer (componente cargado)
-	$lista_apuntes_recientes .= $componente_apunte->getBuffer();
+		// Se guarda el buffer (componente cargado)
+		$lista_apuntes_recientes .= $componente_apunte->getBuffer();
 
-	// Se restaura el componente vacio para ser llenado nuevamente
-	$componente_apunte->restore();
-}
-$tpl->setVar("VISTOS_RECIENTEMENTE", $lista_apuntes_recientes);
+		// Se restaura el componente vacio para ser llenado nuevamente
+		$componente_apunte->restore();
+	}
+	$tpl->setVar("VISTOS_RECIENTEMENTE", $lista_apuntes_recientes);
 
-// ====== PARA TI ======
+	// ====== PARA TI ======
 
-$lista_apuntes_para_ti = "";
+	$lista_apuntes_para_ti = "";
 
-// Carga de info en componentes (debe ser dinámica, osea un foreach con la info que trajo el modelo)
-for ($i = 0; $i < 6; $i++) {
-	// Variables de prueba para los apuntes
-	$vars = [
-		"IMAGEN" => "",
-		"TITULO" => "Titulo",
-		"MATERIA" => "Historia",
-		"ESCUELA" => "Roberto Arlt",
-		"AÑO" => "2025",
-		"PUNTUACION" => "4.5"
-	];
+	// Carga de info en componentes (debe ser dinámica, osea un foreach con la info que trajo el modelo)
+	for ($i = 0; $i < 6; $i++) {
+		// Variables de prueba para los apuntes
+		$vars = [
+			"IMAGEN" => "",
+			"TITULO" => "Titulo",
+			"MATERIA" => "Historia",
+			"ESCUELA" => "Roberto Arlt",
+			"AÑO" => "02/08/2025",
+			"PUNTUACION" => "4.5"
+		];
 
-	// Se carga la info en el buffer componente
-	$componente_apunte->setVars($vars);
+		// Se carga la info en el buffer componente
+		$componente_apunte->setVars($vars);
 
-	// Se guarda el buffer (componente cargado)
-	$lista_apuntes_para_ti .= $componente_apunte->getBuffer();
+		// Se guarda el buffer (componente cargado)
+		$lista_apuntes_para_ti .= $componente_apunte->getBuffer();
 
-	// Se restaura el componente vacio para ser llenado nuevamente
-	$componente_apunte->restore();
-}
-$tpl->setVar("PARA_TI", $lista_apuntes_para_ti);
+		// Se restaura el componente vacio para ser llenado nuevamente
+		$componente_apunte->restore();
+	}
+	$tpl->setVar("PARA_TI", $lista_apuntes_para_ti);
 
-$tpl->print();
+	$tpl->print();
+
+?>
