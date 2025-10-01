e74b520089754d512dc8228533404d909c290c06
Milee025 <milenalopez937@gmail.com>
2025-10-01 19:02:16 -0300

    Ticket 0010 Refactor * explorarView.tpl.php - Se elimino el html correspondiente al sidebar * explorar.css  - se quito el css del sidebar

--- views/explorarView.tpl.php
+++ views/explorarView.tpl.php
@@ -33,27 +33,6 @@
 
     <main>
             <section class="barra_superior">
-                <nav class="sidebar_lateral">
-                    <input type="checkbox" id="sidebar_activo">
-                    <label for="sidebar_activo" class="abrir_sidebar">
-                        <i class="fa-solid fa-bars"></i>
-                    </label>
-                    <div class="escuelas_materias">
-                    <label for="sidebar_activo" class="cerrar_sidebar">
-                        <i class="fa-solid fa-x"></i>
-                    </label>
-
-                    <span class="escuela">Roberto Arlt</span>
-                        <a href="#" class="materia">Literatura</a>
-                        <a href="#" class="materia">Matemática</a>
-                        <a href="#" class="materia">Historia</a>
-
-                    <span class="escuela">Fátima</span>
-                        <a href="#" class="materia">Literatura</a>
-                        <a href="#" class="materia">Matemática</a>
-                        <a href="#" class="materia">Historia</a>
-                    </div>
-                </nav>
                 <section class="buscador_subir_apunte">
                     <div class="contenedor_buscador">
                         <input type="text" placeholder="Buscar..." class="input_buscador">
