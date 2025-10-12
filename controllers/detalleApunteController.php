0b6dcd43d323e268b9ba7fb075851e3142d1ee6b
Joaquin-dv <joaquinpaez315@gmail.com>
2025-10-12 18:57:14 -0300

    ticket 0026 feature *Apuntes.php : se cambia el metodo getApuntes() para que devuelva dos variables nuevas "apunte_id, usuario_id" los cuales despues usa el controller. Se creo el metodo getApunteById() para obtener la info de un apunte al verlo en detalle. se creo el metodo de getRutaApunteById() para usar en el visor de la vista en detalle. Se borro el viejo metodo "getApuntePorId()" por el nuevo "getApunteById()" Se modifico el metodo create() para que guarde en la base de datos la ruta absoluta del archivo, y usa la ruta relativa para guardar el archivo, se usa otra variable de ruta_archiv_fs para pasarle a la IA. se creo el metodo getComentariosByApunte para obtener todos los comentarios segun el apunte_id enviado. Se creo el metodo createComentario() para poder subir un comentario a un apunte.
    *detalleApunteController.php : Se creo toda la logica para asignar los valores de las variables para la vista detalle apunte, con toda la informacion del apunte, junto con los comentarios.
    
    *detalleApunteView.tpl.php : Se cambio informacion hardcodeada por variables, SE DEJO EL PROFESOR HARDCODEADO.
    Y se creo atributo en el visor pdf para asignarlo con el controller, el cual asigna la ruta del archivo para luego leerla con el js. O el error.
    
    *apunteExtends.tpl.php : se modifico la informacion que se envia por el link, para pasar el apunte_id, y asi poder ver cada apunte en detalle, con la informacion real.
    
    +comentarioApunteExtends.tpl.php : Se creo este extend para reemplazar la informacion hardcodeada de los comentarios en detalle apunte, se usa este extend dsp en el controller para ir asignandole valores.
    
    *detalleApunte.css : se modifico dos lineas de codigo para que no se superponga el primer comentario con el label de "subir un comentario".
    
    *detalleApunte.js : Se cambio la logica para informar correctamente por el visor si hubo un error, o mostrar bien el archivo pdf.

--- controllers/detalleApunteController.php
+++ controllers/detalleApunteController.php
@@ -1,12 +1,67 @@
 <?php
-// ============================ CONTROLADOR DETALLE APUNTE ============================
+    // ============================ CONTROLADOR DETALLE APUNTE ============================
 
-// Cargamos el motor de plantillas Mopla
-$tpl = new Mopla("detalleApunte");
+    // Cargamos el motor de plantillas Mopla
+    $tpl = new Mopla("detalleApunte");
 
+    // Se carga el componente
+    $comentarioExtend = new Extend("comentarioApunte");
 
+    // Se carga el modelo de apuntes
+    $apunte = new Apuntes();
 
-// Mostramos la vista
-$tpl->printToScreen();
+    // Cargamos la informacion del apunte
+    $info_apunte = $apunte->getApunteById($_GET['apunteId'], true)[0];
+
+    // Array para guardar el componente con la informacion cargada
+    $lista_comentarios = "";
+
+    // Se carga el modelo de comentarios
+    $lista_comentarios_apunte = $apunte->getComentariosByApunte($_GET['apunteId']);
+
+    // Obtener la ruta del archivo del apunte
+    $ruta_archivo_result = $apunte->getRutaApunteById($_GET['apunteId']);
+
+    if(is_array($ruta_archivo_result) && isset($ruta_archivo_result['errno'])){
+        $ruta_archivo = "";
+        $error_archivo = "No se encontro el archivo";
+    } else {
+        $ruta_archivo = $ruta_archivo_result;
+        $error_archivo = "";
+    }
+
+    // Manejar envío de comentario
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['comentario'])) {
+        $resultado = $apunte->createComentario($_GET['apunteId'], $_POST['comentario']);
+        if ($resultado['errno'] === 201) {
+            // Recargar comentarios después de crear uno nuevo
+            $lista_comentarios_apunte = $apunte->getComentariosByApunte($_GET['apunteId']);
+        }
+    }
+    // Incrementar las visitas del apunte (descomentariar para probar)
+    // $apunte->incrementarVisitas($_GET['apunteId']);
+
+    // Cargo la informacion en los componentes
+    foreach ($lista_comentarios_apunte as $row) {
+        $lista_comentarios .= $comentarioExtend->assignVar($row);
+    }
+
+    if (empty($lista_comentarios)) {
+        $lista_comentarios = '<p class="msg_vacio">No hay comentarios del apunte</p>';
+    }
+
+    // Verificamos que el apunte exista    DESCOMENTAR ESTO PARA PROBAR
+    // if (!$info_apunte) {
+    //     // Redirigir a una página de error o mostrar un mensaje
+    //     header("Location: ?slug=errorApunteNoEncontrado");
+    //     exit();
+    // }
+
+    // Cargamos los componentes necesarios
+    $tpl->assignVar(["TITULO" => $info_apunte['TITULO'], "MATERIA" => $info_apunte['MATERIA'], "ESCUELA" => $info_apunte['ESCUELA'], "AÑO" => $info_apunte['AÑO'], "PROMEDIO_CALIFICACIONES" => $info_apunte['PROMEDIO_CALIFICACIONES'], "CANTIDAD_PUNTUACIONES" => $info_apunte['CANTIDAD_PUNTUACIONES'], "NOMBRE_USUARIO" => $info_apunte['NOMBRE_USUARIO'], "FECHA_CREACION" => $info_apunte['FECHA_CREACION'], "RUTA_ARCHIVO" => $ruta_archivo, "ERROR_ARCHIVO" => $error_archivo]);
+    $tpl->assignVar(["COMENTARIOS_APUNTE" => $lista_comentarios]);
+    
+    // Mostramos la vista
+    $tpl->printToScreen();
 
 ?>
